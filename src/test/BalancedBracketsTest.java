c7a5602b3beb0978f8868e2adf753472f4c5371e
Susan <susandu924@gmail.com>
2022-08-29 23:21:24 -0500

    Unit Testing Studio complete

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,70 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
+    //    TODO: add tests here
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void twoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+
+    @Test
+    public void oneBracketWithinTwoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void oneBracketWithinTwoBracketsAndOneBracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]][]"));
+    }
+
+    @Test
+    public void TripleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+
+    @Test
+    public void TwoBracketsWithinDoublesReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[][]]]"));
+    }
+    @Test
+    public void EmptyQuotesReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void PizzaWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[pizza]"));
+    }
+    @Test
+    public void OneOpenBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void OneClosedBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void BackwardsBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void PizzaWithExtraOpenBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[pizza[]"));
+    }
 
 }
